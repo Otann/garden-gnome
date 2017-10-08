37af911890cb87027f8c312ad7544c1be6a8c8eb
Anton Chebotaev <anton.chebotaev@gmail.com>
2017-10-08 15:44:40 +0200

    initial commit: lein new app garden-mount

---
+++ src/garden_mount/core.clj
@@ -0,0 +1,7 @@
+(ns garden-mount.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/garden_mount/core_test.clj
@@ -0,0 +1,7 @@
+(ns garden-mount.core-test
+  (:require [clojure.test :refer :all]
+            [garden-mount.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
